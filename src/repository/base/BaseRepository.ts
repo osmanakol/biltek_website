70beb4694f0e3e8c2a3f33f180cdd0f78b56296f
osmanakol <osmanakol@outlook.com>
2020-04-26 23:23:35 +0300

    create and update function

--- src/repository/base/BaseRepository.ts
+++ src/repository/base/BaseRepository.ts
@@ -18,11 +18,14 @@ export abstract class BaseRepository<T> implements IWrite<T>, IRead<T>{
     findById(id: string): Promise<T> {
         throw new Error("Method not implemented.");
     }
-    create(item: T): Promise<import("mongoose").Document> {
-        throw new Error("Method not implemented.");
+    create(item: T): Promise<Document> {
+        const result = this._model.create(item);
+        return result;
     }
-    update(id: string, item: T): Promise<boolean> {
-        throw new Error("Method not implemented.");
+    update(id: string, item: T): Promise<Document> {
+        const result = this._model.update({"_id":id},item);
+        console.log(result);
+        return result as any;
     }
     delete(id: string): Promise<boolean> {
         throw new Error("Method not implemented.");
