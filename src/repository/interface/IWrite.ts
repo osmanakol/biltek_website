83cc0f90d782eac72a210753bcb89e4306d40607
osmanakol <osmanakol@outlook.com>
2020-04-26 23:23:17 +0300

    update function return type should be promise document

--- src/repository/interface/IWrite.ts
+++ src/repository/interface/IWrite.ts
@@ -2,6 +2,6 @@ import { Document } from "mongoose";
 
 export interface IWrite<T>{
     create(item:T):Promise<Document>;
-    update(id:string,item:T):Promise<boolean>
+    update(id:string,item:T):Promise<Document>
     delete(id:string):Promise<boolean>;
 }
