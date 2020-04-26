1d7f07d706b08d43b306dfdcdf24d86dfa9d529c
osmanakol <osmanakol@outlook.com>
2020-04-27 00:19:35 +0300

    ParticipantRepository extends to BaseRepository

---
+++ src/repository/ParticipantRepository.ts
@@ -0,0 +1,6 @@
+import { BaseRepository } from "./base/BaseRepository";
+import { ParticipantModel } from "../models/participants/participantsModel";
+
+export class ParticipantRepository extends BaseRepository<ParticipantModel>{
+
+}
