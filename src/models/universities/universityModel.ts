4aa11155d5f9ada459b9ad8c20ef58254f83fdc0
osmanakol <osmanakol@outlook.com>
2020-04-27 22:53:07 +0300

    universityModel design

--- src/models/universities/universityModel.ts
+++ src/models/universities/universityModel.ts
@@ -1,4 +1,10 @@
 
 export class UniversityModel {
     // TODO : Propertyler belirlenecek, constructor yazılacak ve participant model de bu class ile ilgili değişikler yapılacak.
+    public universityName:string;
+
+    constructor(universityName:string) {
+        this.universityName = universityName;
+    }
+    
 }
