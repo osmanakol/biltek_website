470a7717e76103b01847a06fd946d973ae98030a
osmanakol <osmanakol@outlook.com>
2020-04-27 23:23:04 +0300

    participantService create

---
+++ src/services/UniversityService.ts
@@ -0,0 +1,18 @@
+import { UniversityRepository } from "../repository/UniversityRepository";
+import UniversityDbModel from "../models/universities/universitySchema";
+import { UniversityModel } from "../models/universities/universityModel";
+
+export class UniversityService{
+    private universityRepository:UniversityRepository = new UniversityRepository(UniversityDbModel);
+
+    constructor() {
+        
+    }
+
+    public create = async (item:UniversityModel) =>{
+        console.log("Service");
+        const result =await this.universityRepository.create(item);
+        return result;
+    }
+
+}
