bae7935f971d28585d4cf7c0236a588811148e0c
osmanakol <osmanakol@outlook.com>
2020-04-29 22:17:12 +0300

    add createMany Methods

--- src/controller/UniversityController.ts
+++ src/controller/UniversityController.ts
@@ -1,22 +1,38 @@
 import { UniversityService } from "../services/UniversityService";
-import { Request,Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import { UniversityModel } from "../models/universities/universityModel";
 
-export class UniversityController{
-    private universityService:UniversityService = new UniversityService();
+export class UniversityController {
+    private universityService: UniversityService = new UniversityService();
 
-    public createUniversity = async (req:Request,res:Response,next:NextFunction)=>{
+    public createUniversity = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const result = await this.universityService.create(new UniversityModel(req.body.name))
             res.status(201).json({
-                data:result,
-                message:"Ok"
+                data: result,
+                message: "Ok"
             })
         } catch (error) {
             res.json({
-                err:error,
-                message:"Bir hata oluştu"
+                err: error,
+                message: "Bir hata oluştu"
             })
         }
     }
+
+    public createUniversities = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.universityService.createMany(new Array<UniversityModel>(...req.body.universities))
+            res.status(201).json({
+                data:result,
+                message:"OK"
+            })
+        } catch (error) {
+            res.json({
+                err: error,
+                message: "Bir hata oluştu"
+            })
+        } 
+    }
+
 }
