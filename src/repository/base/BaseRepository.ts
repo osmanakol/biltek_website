b80646c4d5687774fe51032da03dbbbd804ee0b0
osmanakol <osmanakol@outlook.com>
2020-04-29 22:16:51 +0300

    write comment lines and delete, update,findbyId,findall,createMany function

--- src/repository/base/BaseRepository.ts
+++ src/repository/base/BaseRepository.ts
@@ -1,34 +1,54 @@
 import { IRead } from "../interface/IRead";
 import { IWrite } from "../interface/IWrite";
 import { Model,Document } from "mongoose";
+import { STATUS_CODES } from "http";
 export abstract class BaseRepository<T> implements IWrite<T>, IRead<T>{
-
+    
     public readonly _model:Model<Document>
+
+    /**
+     * @constructor
+     * @param model is exporting to models file
+     */
 
     constructor(model:Model<Document>) {
         this._model = model;
     }
 
+    //! That functions commets line is in the their interface file (IRead and IWrite)
+
+    createMany(item: T[]): Promise<Document[]> {
+        const result = this._model.create(item);
+        return result;
+    }
+
     findAll(): Promise<T[]> {
+        const result = this._model.find({})
+        return result.cast(this._model);
+    }
+
+    findOne(property: any): Promise<T> {
         throw new Error("Method not implemented.");
     }
-    findOne(propert: any): Promise<T> {
-        throw new Error("Method not implemented.");
-    }
+
     findById(id: string): Promise<T> {
-        throw new Error("Method not implemented.");
+        const result = this._model.findById({"_id":id});
+        return result.cast(this._model);
     }
+
     create(item: T): Promise<Document> {
         const result = this._model.create(item);
         return result;
     }
+    
     update(id: string, item: T): Promise<Document> {
         const result = this._model.update({"_id":id},item);
         console.log(result);
         return result as any;
     }
-    delete(id: string): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
 
+    delete(id: string): Promise<boolean> {
+        const result = this._model.deleteOne({"_id":id});
+        return result as any;
+    }
 }
