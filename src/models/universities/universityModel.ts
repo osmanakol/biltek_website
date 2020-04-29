499393401e09f61c21c7855d721e3d04266e2b8a
osmanakol <osmanakol@outlook.com>
2020-04-29 22:42:24 +0300

    entegration of department for universities

--- src/models/universities/universityModel.ts
+++ src/models/universities/universityModel.ts
@@ -1,10 +1,13 @@
+import { DepartmentModel } from "../departmentOfUniversity/departmentModel";
 
 export class UniversityModel {
     // TODO : Propertyler belirlenecek, constructor yazılacak ve participant model de bu class ile ilgili değişikler yapılacak.
     public universityName:string;
+    public department:DepartmentModel[]
 
-    constructor(universityName:string) {
+    constructor(universityName:string,department:DepartmentModel[]) {
         this.universityName = universityName;
+        this.department = department;
     }
     
 }
