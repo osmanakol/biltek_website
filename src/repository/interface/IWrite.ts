1a24dbd5af7252652d2fbe51b8102cfcbcfd221c
osmanakol <osmanakol@outlook.com>
2020-04-29 15:16:23 +0300

    IWrite.ts added  comments lines

--- src/repository/interface/IWrite.ts
+++ src/repository/interface/IWrite.ts
@@ -1,7 +1,35 @@
+
 import { Document } from "mongoose";
 
+
 export interface IWrite<T>{
+    /**
+     * @function @async 
+     * @param item[] 
+     * @return Document[] what is created
+     * ? create more then one data in database
+     */
+    createMany(item:T[]):Promise<Document[]>;
+    /**
+     * @function @async
+     * @param item 
+     * @return Document what is created
+     * ? create a data in database
+     */
     create(item:T):Promise<Document>;
+    /**
+     * @function @async
+     * @param id which document you want to update
+     * @param item update value
+     * @return Document which is updated 
+     * ? update any document in  database
+     */
     update(id:string,item:T):Promise<Document>
+
+    /**
+     * @function @async
+     * @param id which document you want to delete
+     * @returns boolean result about delete process
+     */
     delete(id:string):Promise<boolean>;
 }
