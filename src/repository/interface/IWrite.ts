a93072e6e6116b9f5e5a7d14ce4e414dc5d4106b
osmanakol <osmanakol@outlook.com>
2020-04-29 16:59:55 +0300

    edit comments line

--- src/repository/interface/IWrite.ts
+++ src/repository/interface/IWrite.ts
@@ -22,7 +22,7 @@ export interface IWrite<T>{
      * @param id which document you want to update
      * @param item update value
      * @return Document which is updated 
-     * ? update any document in  database
+     * ? update any document in database
      */
     update(id:string,item:T):Promise<Document>
 
@@ -30,6 +30,7 @@ export interface IWrite<T>{
      * @function @async
      * @param id which document you want to delete
      * @returns boolean result about delete process
+     * ? delete any document in database
      */
     delete(id:string):Promise<boolean>;
 }
