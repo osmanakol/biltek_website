78195b04744bad7c4365cdcd9d8449ee48136576
osmanakol <osmanakol@outlook.com>
2020-04-29 22:15:51 +0300

    createMany function

--- src/services/UniversityService.ts
+++ src/services/UniversityService.ts
@@ -2,16 +2,23 @@ import { UniversityRepository } from "../repository/UniversityRepository";
 import UniversityDbModel from "../models/universities/universitySchema";
 import { UniversityModel } from "../models/universities/universityModel";
 
-export class UniversityService{
-    private universityRepository:UniversityRepository = new UniversityRepository(UniversityDbModel);
+export class UniversityService {
+    private universityRepository: UniversityRepository = new UniversityRepository(UniversityDbModel);
 
     constructor() {
-        
+
     }
 
-    public create = async (item:UniversityModel) =>{
+    public create = async (item: UniversityModel) => {
         console.log("Service");
-        const result =await this.universityRepository.create(item);
+        const result = await this.universityRepository.create(item);
+        return result;
+    }
+
+    public createMany = async (item: UniversityModel[]) => {
+        console.log("Service");
+        const result = await this.universityRepository.createMany(item);
+        console.log(result);
         return result;
     }
 
