e05d74765ab21e9da25b8dff961a8f1400a1e377
osmanakol <osmanakol@outlook.com>
2020-05-02 12:36:48 +0300

    write all function

--- src/services/UniversityService.ts
+++ src/services/UniversityService.ts
@@ -3,7 +3,7 @@ import UniversityDbModel from "../models/universities/universitySchema";
 import { UniversityModel } from "../models/universities/universityModel";
 
 export class UniversityService {
-    private universityRepository: UniversityRepository = new UniversityRepository(UniversityDbModel);
+    private repository: UniversityRepository = new UniversityRepository(UniversityDbModel);
 
     constructor() {
 
@@ -11,14 +11,35 @@ export class UniversityService {
 
     public create = async (item: UniversityModel) => {
         console.log("Service");
-        const result = await this.universityRepository.create(item);
+        const result = await this.repository.create(item);
+        return result;
+    }
+
+    public update = async (id:string,item:UniversityModel) =>{
+        const result = await this.repository.update(id,item);
+        return result;
+    }
+
+    public delete = async (id:string)=>{
+        const result = await this.repository.delete(id);
         return result;
     }
 
     public createMany = async (item: UniversityModel[]) => {
         console.log("Service");
-        const result = await this.universityRepository.createMany(item);
+        const result = await this.repository.createMany(item);
         console.log(result);
+        return result;
+    }
+
+    public findById = async (id:string)=>{
+        const result = await this.repository.findById(id);
+        return result;
+    }
+
+    public findAll = async ()=>{
+        console.log("Service")
+        const result = await this.repository.findAll();
         return result;
     }
 
