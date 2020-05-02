6aee5b6b7e1bacf1c8f742fd2d918f820e73d148
osmanakol <osmanakol@outlook.com>
2020-05-02 12:32:51 +0300

    finish all function of baserepository

--- src/controller/ParticipantController.ts
+++ src/controller/ParticipantController.ts
@@ -4,19 +4,98 @@ import { ParticipantModel } from "../models/participants/participantModel";
 import { UniversityModel } from "../models/universities/universityModel";
 
 export class ParticipantController {
+    private participantService: ParticipantService;
+
+    constructor() {
+        this.participantService = new ParticipantService()
+    }
     public createParticipant = async (req: Request, res: Response, next: NextFunction) => {
-        const participantService = new ParticipantService();
-        console.log(req.body);
-        const result = await participantService.create(new ParticipantModel(req.body.name, req.body.surName, new UniversityModel(req.body.universityModel.name), req.body.email)).then((result) => {
+
+        const result = await this.participantService.create(new ParticipantModel(req.body.name, req.body.surName, new UniversityModel(req.body.universityName), req.body.email)).then((result) => {
             res.status(201).json({
                 data: result,
                 message: "OK"
             })
         }).catch(err => {
             res.json({
-                err:err,
+                err: err,
                 message: "Bir hata oluştu"
             })
         })
+    }
+
+    public createManyParticipant = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.participantService.createMany(new Array<ParticipantModel>(...req.body.participants))
+            res.status(201).json({
+                data: result,
+                state: "Success"
+            })
+        } catch (error) {
+            res.json({
+                err: error,
+                state: "Error"
+            })
+        }
+    }
+
+    public updateParticipant = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.participantService.update(req.body._id, new ParticipantModel(req.body.name, req.body.surName, new UniversityModel(req.body.universityName), req.body.email))
+            res.status(200).json({
+                data: result,
+                state: "Success"
+            })
+        } catch (error) {
+            res.json({
+                err: error,
+                state: "Error"
+            })
+        }
+    }
+
+    public deleteParticipant = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.participantService.delete(req.body._id)
+            res.status(200).json({
+                result:result,
+                state:"Success"
+            })
+        } catch (error) {
+            res.json({
+                err:error,
+                state:"Error"
+            })
+        }
+    }
+
+    public findAll = async (req:Request,res:Response,next:NextFunction)=>{
+        try {
+            const result = await this.participantService.findAll()
+            res.json({
+                data:result,
+                state:"Success"
+            })
+        } catch (error) {
+            res.json({
+                err:error,
+                state:"Error"
+            })
+        }
+    }
+
+    public findById = async(req:Request,res:Response,next:NextFunction)=>{
+        try {
+            const result = await this.participantService.findById(req.body._id)
+            res.json({
+                data:result,
+                state:"Success"
+            })
+        } catch (error) {
+            res.json({
+                err:error,
+                state:"Error"
+            })
+        }
     }
 }
