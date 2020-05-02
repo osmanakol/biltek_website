950d4bebf6e542d61938d798791907ff8cd2fe1b
osmanakol <osmanakol@outlook.com>
2020-05-02 12:35:00 +0300

    add participantModel for repository

--- src/repository/ParticipantRepository.ts
+++ src/repository/ParticipantRepository.ts
@@ -1,5 +1,5 @@
 import { BaseRepository } from "./base/BaseRepository";
-import { ParticipantModel } from "../models/participants/participantsModel";
+import { ParticipantModel } from "../models/participants/participantModel";
 
 export class ParticipantRepository extends BaseRepository<ParticipantModel>{
 
