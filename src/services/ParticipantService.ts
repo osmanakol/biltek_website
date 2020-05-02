e48f63a4a16cb2e084e396e1f470041617ca4013
osmanakol <osmanakol@outlook.com>
2020-05-02 12:36:35 +0300

    write all function

--- src/services/ParticipantService.ts
+++ src/services/ParticipantService.ts
@@ -1,6 +1,6 @@
 import { ParticipantRepository } from "../repository/ParticipantRepository";
 import ParticipantDbModel from "../models/participants/participantSchema";
-import { ParticipantModel } from "../models/participants/participantsModel";
+import { ParticipantModel } from "../models/participants/participantModel";
 
 export class ParticipantService {
     private repository = new ParticipantRepository(ParticipantDbModel);
@@ -12,4 +12,36 @@ export class ParticipantService {
         const result = await this.repository.create(item);
         return result;
     }
+
+    public createMany = async (item:ParticipantModel[])=>{
+        console.log("Service")
+        const result = await this.repository.createMany(item)
+        return result;
+    }
+
+    public update = async(id:string,item:ParticipantModel)=>{
+        console.log("Service")
+        const result = await this.repository.update(id,item);
+        return result;
+    }
+
+    public delete = async(id:string)=>{
+        console.log("Service")
+        const result = await this.repository.delete(id);
+        return result;
+    }
+
+    public findAll=async()=>{
+        console.log("Service")
+        const result = await this.repository.findAll()
+        return result;
+    }
+
+    public findById = async(id:string)=>{
+        console.log("Service")
+        const result = await this.repository.findById(id);
+        return result;
+    }
+
+     
 }
