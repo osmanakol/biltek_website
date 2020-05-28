880c53ec75a2d5dd760363ff0a5412d703991fc2
velikrgl <velikrgl@yahoo.com>
2020-05-28 16:21:03 +0300

    buraları ben yaptım hep işte

---
+++ static/register/js/register.js
@@ -0,0 +1,21 @@
+$("div[id^='myModal']").each(function(){
+  
+    var currentModal = $(this);
+    //click next
+    currentModal.find('.btn-success').click(function(){
+      currentModal.modal('hide');
+      currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show'); 
+    });
+    
+    //click prev
+    currentModal.find('.btn-danger').click(function(){
+      currentModal.modal('hide');
+      currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show');   
+    });
+  
+  }); 
+
+
+
+
+
