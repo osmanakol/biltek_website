d19783884d6c26b4fdc6bdc92e7866540408e5c1
osmanakol <osmanakol@outlook.com>
2020-06-06 20:35:34 +0300

    revert

--- dene.ts
+++ dene.ts
@@ -5,10 +5,4 @@ console.log(dene);
 console.assert(true,"asdasd");
 console.warn("uyarı işte")
 console.warn("deneme işte")
-<<<<<<< HEAD
 console.clear()
-console.error("son deneme")
-console.error("Oğuzhanın kafa bozuk")
-=======
-console.clear()
->>>>>>> parent of 1f1bc5e... slack denem3
