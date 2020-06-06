788c2b964377970f33bb7c3e38b50ba1abc33c51
osmanakol <osmanakol@outlook.com>
2020-06-06 20:45:58 +0300

    Revert "make necessary functions commit"
    This reverts commit 001cfc1c36ad55cc6bc8a4f2ec8a9e2127843228.

--- static/register/js/register.js
+++ static/register/js/register.js
@@ -1,106 +1,13 @@
+$("div[id^='myModal']").each(function () {
 
-function next_Prev(n) {
-
-  const name = document.forms["myForm"]["form-first-name"].value;
-  const lastname = document.forms["myForm"]["form-last-name"].value
-  const tel = document.forms["myForm2"]["form-phone"].value;
-  const email = document.forms["myForm2"]["form-adress"].value;
-  if (n == 1) {
-    $("div[id^='myModal']").each(function () {
-      var currentModal = $(this);
-      currentModal.find('#next-button1').click(function () {
-
-        if (name == "" || lastname == "") {
-          console.log("tabi1if");
-          document.getElementById('adsL').innerHTML
-            = 'Alanları doldur!';
-          return false;
-
-        }
-        else {
-          console.log("tab1else");
-          document.getElementById('adsL').innerHTML
-            = '';
-          currentModal.modal('hide');
-          currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show');
-
-        }
-
-        
-      });
-    });
-  }
-
-  if (n == 2) {
-    $("div[id^='myModal']").each(function () {
-
-      var currentModal = $(this);
-      //click next
-      currentModal.find('#next-button2').click(function () {
-
-
-
-        if (tel == "" || email == "") {
-          console.log("tab2if");
-          document.getElementById('IL').innerHTML
-            = 'Boslukları doldurun!';
-          return false;
-
-        }
-        else {
-          console.log("tab2else");
-          document.getElementById('IL').innerHTML
-            = '';
-          currentModal.modal('hide');
-          currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show');
-
-        }
-      });
-
-      currentModal.find('#next-buttonGeri').click(function () {
-        currentModal.modal('hide');
-        currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show');
-      });
-
-
-
-    });
-
-
-
-
-  }
-}
-
-
-
-
-/*
-function next_prev() {
-  $("div[id^='myModal1']").each(function () {
-
-    var name = document.forms["myForm"]["form-first-name"].value;
- var lastname = document.forms["myForm"]["form-last-name"].value
-    var currentModal = $(this);
-    //click next
-    currentModal.find('.btn-success').click(function () {
-
-      if (name == "" || lastname=="") {
-        document.getElementById('adsL').innerHTML
-        = 'Alanları doldur!';
-        return false;
-
-      }
-      else {
-        document.getElementById('adsL').innerHTML
-        = '';
-        currentModal.modal('hide');
-        currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show');
-      }
-    });
+  var currentModal = $(this);
+  //click next
+  currentModal.find('.btn-success').click(function () {
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show');
   });
 }
-*/
+
 
 /*
 function next_prev2(){
@@ -137,12 +44,6 @@ function next_prev2(){
 
   });
 
+});
 
-
-
-
-
-
-}
-*/
 //TODO: Fonksiyion içine koyulacak
