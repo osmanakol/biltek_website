f182a1f92b36da8f78f68ad02846222a6d2e9449
velikrgl <velikrgl@yahoo.com>
2020-06-27 16:37:35 +0300

    button animation and submit button affect done

--- static/register/js/register.js
+++ static/register/js/register.js
@@ -80,74 +80,82 @@ function getDepartmentsById() {
 }
 
 /*Form submit validation*/
-function submit_validation(){
-    
+/*Form submit validation*/
+function submit_validation() {
+    $("#mymodal1").hide;
     const email = document.getElementById("email");
     const emailPattern = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/;
-    
-var name_surname=$("#id_name").val();
-var phone=$("#id_phone").val();
 
-var school=document.getElementById("id_school");
-var text_school=school.options[school.selectedIndex].text;
-var dep=document.getElementById("id_dep");
-var text_dep=dep.options[dep.selectedIndex].text;
-
-if(text_dep=="Bölümünüzü seçiniz..."){
-    swal({
-        title: "Boşlukları doldurun !",
-        text: "Bölüm boş bırakılamaz ! ",
-        icon: "warning",
-        button: "Tamam",
-      });
-
-}
+    const emailBox = document.querySelector('.emailBox');
 
 
-if(text_school=="Okulunuzu seçiniz..."){
-    swal({
-        title: "Boşlukları doldurun !",
-        text: "Okul boş bırakılamaz ! ",
-        icon: "warning",
-        button: "Tamam",
-      });
+    var name_surname = $("#id_name").val();
+    var phone = $("#id_phone").val();
 
-}
+    var school = document.getElementById("id_school");
+    var text_school = school.options[school.selectedIndex].text;
+    var dep = document.getElementById("id_dep");
+    var text_dep = dep.options[dep.selectedIndex].text;
 
-if(!email.value.match(emailPattern)){
-    swal({
-        title: "Boşlukları doldurun !",
-        text: "Geçersiz mail adresi ! ",
-        icon: "warning",
-        button: "Tamam",
-      });
+    if (text_dep == "Bölümünüzü seçiniz...") {
+        swal({
+            title: "Boşlukları doldurun !",
+            text: "Bölüm boş bırakılamaz ! ",
+            icon: "warning",
+            button: "Tamam",
+        });
 
-
-}
-
-if(name_surname=="")
-{
-    swal({
-        title: "Boşlukları doldurun !",
-        text: "Ad soyad boş bırakılamaz ! ",
-        icon: "warning",
-        button: "Tamam",
-      });
-}
+    }
 
 
+    if (text_school == "Okulunuzu seçiniz...") {
+        swal({
+            title: "Boşlukları doldurun !",
+            text: "Okul boş bırakılamaz ! ",
+            icon: "warning",
+            button: "Tamam",
+        });
 
-if(name_surname!="" && email.value.match(emailPattern) && text_school!="Okulunuzu seçiniz..." && text_dep!="Bölümünüzü seçiniz..." ){
-    
-    swal({
-        title: "Kayıt başarılı",
-        text: "Kayıt olduğunuz için teşekkürler!",
-        icon: "success",
-        button: "Tamam",
-        
-      });
-      $('#myModal1').modal('hide');
-}
+    }
 
+    if (!email.value.match(emailPattern)) {
+        swal({
+            title: "Boşlukları doldurun !",
+            text: "Geçersiz mail adresi ! ",
+            icon: "warning",
+            button: "Tamam",
+        });
+
+
+    }
+
+    if (name_surname == "") {
+        swal({
+            title: "Boşlukları doldurun !",
+            text: "Ad soyad boş bırakılamaz ! ",
+            icon: "warning",
+            button: "Tamam",
+        });
+    }
+
+
+
+    if (name_surname != "" && email.value.match(emailPattern) && text_school != "Okulunuzu seçiniz..." && text_dep != "Bölümünüzü seçiniz...") {
+
+        Swal.fire
+            ({
+                icon: 'success',
+                title: 'Kayıt başarılı',
+                showConfirmButton: false,
+                timer: 1500
+            });
+
+        emailBox.classList.remove('invalid');
+        emailBox.classList.remove('valid');
+
+        document.getElementById("form").reset();
+        $('#myModal1').modal('hide');
+
+    }
 
 }
