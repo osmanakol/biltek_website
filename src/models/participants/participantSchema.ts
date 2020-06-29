566111983f116f55c9e2abc9ba6e01e0ea40ba61
velikrgl <velikrgl@yahoo.com>
2020-06-29 15:37:25 +0300

    participant shema fix

--- src/models/participants/participantSchema.ts
+++ src/models/participants/participantSchema.ts
@@ -1,20 +1,20 @@
 import { Document, Model, model, Schema } from "mongoose";
 
 export interface IParticipant extends Document {
-    name: string;
-    surName: string;
+    name_surname: string;
     university:string;
     department:string;
     email: string;
+    phone:string;
     date:string;
 }
 
 const ParticipantSchema: Schema = new Schema({
-    name: { type: String, required: 'Name is required', trim: true },
-    surName: { type: String, required: 'Surname is required parameter', trim: true },
+    name_surname: { type: String, required: 'Name is required', trim: true },
     university: { type: String, required:'University is required parameter'},
     department:{type:String,required:'Department is required parameter'},
     email: { type: String, required: 'Email is a required parameter', trim: true },  
+    phone:{type:String},
     date:{type:String,required:"Date is a required parameter",default:new Date().toLocaleDateString("tr-TR",{timeZone:"Europe/Istanbul",weekday:"long",year:"numeric",month:"short",day:"numeric"})}
 }) 
 const ParticipantDbModel: Model<IParticipant> = model('partipacipants', ParticipantSchema);
