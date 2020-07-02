58aad19110ae76efd2833950bf5204b4cedc87ff
Auskren <kbberkuk@gmail.com>
2020-07-03 00:54:39 +0300

    changed clubname look and timeout timings for guidance

--- errorpage.js
+++ errorpage.js
@@ -5,7 +5,7 @@ const text = document.querySelector(".errorpage-box-terminal-username");
 setTimeout(() => {
     homepage_box.classList.toggle("active");
     terminal_box.classList.toggle("active");
-}, 4000);
+}, 3000);
 setTimeout(() =>{
     window.open("https://www.youtube.com/watch?v=UyceHuMadr8&feature=youtu.be","_self");
 }, 5000);
