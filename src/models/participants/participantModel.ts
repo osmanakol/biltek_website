1b8da41845097b50043817af513dffe0c069530e
osmanakol <osmanakol@outlook.com>
2020-07-18 20:33:02 +0300

    delete check existing mail method and move to mongo schema

--- src/models/participants/participantModel.ts
+++ src/models/participants/participantModel.ts
@@ -1,76 +1,74 @@
-import { UniversityModel } from "../universities/universityModel";
-import { checkSchema} from "express-validator";
-import ParticipantDbModel from "./participantSchema";
+import { checkSchema } from "express-validator"
 
 export class ParticipantModel {
     private name_surname: string;
     public university: string;
-    public department:string;
+    public department: string;
     public email: string;
-    public date:string;
-    public phone?:string;
+    public date: string;
+    public phone?: string;
 
-    constructor(name_surname:string, university: string,department:string, email: string,phone?:string,date=new Date().toLocaleDateString("tr-TR",{timeZone:"Europe/Istanbul",weekday:"long",year:"numeric",month:"short",day:"numeric"})) {
+    constructor(name_surname: string, university: string, department: string, email: string, phone?: string, date = new Date().toLocaleDateString("tr-TR", { timeZone: "Europe/Istanbul", weekday: "long", year: "numeric", month: "short", day: "numeric" })) {
         this.name_surname = name_surname;
         this.university = university;
         this.department = department;
         this.email = email;
-        this.phone=phone;
-        this.date=date;
-     
+        this.phone = phone;
+        this.date = date;
+
     }
 
     public get FullName(): string {
         return this.name_surname.trim();
     }
-    
+
 }
 
-export const ParticipantValidationChain=checkSchema({ 
-    name_surname:{
-        exists:true,
-        errorMessage:"Name_surname propertysi eksik",
-        trim:true,
-        escape:true,
-        isAlpha:{
-            errorMessage:"Invalid name_surname"
+export const ParticipantValidationChain = checkSchema({
+    name_surname: {
+        exists: true,
+        errorMessage: "Name_surname propertysi eksik",
+        trim: true,
+        escape: true,
+        isAlpha: {
+            errorMessage: "Invalid name_surname"
         },
-        isLength:{
-            options:{min:5,max:30},
-            errorMessage:"Minimum 5 characters required!"
+        isLength: {
+            options: { min: 5, max: 30 },
+            errorMessage: "Minimum 5 characters required!"
         },
-        customSanitizer:{
-            options:(value:string,{req})=>{
-                if(req.body.name_surname)
+        customSanitizer: {
+            options: (value: string, { req }) => {
+                if (req.body.name_surname)
                     return value.toUpperCase()
             }
-        }  
-    },
-    email:{
-        exists:{
-            errorMessage:"Email property eksik"
-        },
-        trim:true,
-        isEmail:true,
-        errorMessage:"Invalid email",
-        custom:{
-            options:async (value:string)=>{
-                await ParticipantDbModel.findOne({email:value}).then(query =>{
-                     if (query)
-                        return Promise.reject('E-mail already exist'); 
-                 });
-            },
         }
     },
-    phone:{
-        exists:{
-            errorMessage:"Phone property eksik"
+    email: {
+        exists: {
+            errorMessage: "Email property eksik"
         },
-        blacklist:{
-            options:['-']
+        trim: true,
+        isEmail: true,
+        errorMessage: "Invalid email",
+       /* custom: {
+            options: async (value: string) => {
+                await ParticipantDbModel.findOne({ email: value }).then(query => {
+                    if (query)
+                        return Promise.reject('E-mail already exist');
+                });
+            },
+        }*/
+    },
+    phone: {
+        exists: {
+            errorMessage: "Phone property eksik"
         },
-        optional:{
-            options:{nullable:true}
+        blacklist: {
+            options: ['-']
+        },
+        optional: {
+            options: { nullable: true }
         },
         /*custom:{
             options:(value:string)=>{
@@ -81,15 +79,15 @@ export const ParticipantValidationChain=checkSchema({
             },
             errorMessage:"Invalid Phone Number"
         },*/
-        
-        
+
+
     },
-    university:{
-        exists:true,
-        errorMessage:"University property eksik"
+    university: {
+        exists: true,
+        errorMessage: "University property eksik"
     },
-    department:{
-        exists:true,
-        errorMessage:"Department property eksik"
+    department: {
+        exists: true,
+        errorMessage: "Department property eksik"
     }
 })
