6e2e45727ffcf88a5c6ef872d538b8c0b6007a18
Auskren <kbberkuk@gmail.com>
2020-07-18 21:42:02 +0300

    created MailContent class

--- src/helpers/sendMail.ts
+++ src/helpers/sendMail.ts
@@ -1,7 +1,9 @@
 import {Mailing} from "./Mailing"
+
 try{
     let admin = new Mailing("paaysenur@gmail.com", ".adgjmp.");
     var adress=["aysenuryeter@gmail.com","paaysenur@hotmail.com","paaysenur@gmail.com"];
+    admin.createMessage(adress, "message.html")
     admin.sendMail(adress);
 }
 catch(err){
