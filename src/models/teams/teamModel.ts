6e83eca6487caf53fed2ed567b87d5dec15cc52e
velikrgl <velikrgl@yahoo.com>
2020-07-21 21:09:37 +0300

    team model created

---
+++ src/models/teams/teamModel.ts
@@ -0,0 +1,25 @@
+import { ParticipantModel } from "../participants/participantModel";
+
+
+interface IProject {
+    projectName: string;
+    description: string;
+    year: number;
+}
+type role = "Leader" | "Member"
+
+interface ITeamMember {
+    teamMember: ParticipantModel;
+    year: number
+    role: role;
+}
+
+export class TeamModel {
+    constructor(public teamName: string, public teamMember: ITeamMember[], 
+        public foundationYear: number = new Date().getFullYear(), public isActive: boolean = true) {
+
+    }
+
+
+
+}
