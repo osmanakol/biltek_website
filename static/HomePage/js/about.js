c35f3be3fc15c346a25fd68b39cfd434e39e6e81
Auskren <kbberkuk@gmail.com>
2020-07-25 18:37:31 +0300

    horizontal extend version of about boxes

--- static/HomePage/js/about.js
+++ static/HomePage/js/about.js
@@ -1,6 +1,12 @@
 const box_one = document.querySelector(".about-header-box-one")
 const box_two = document.querySelector(".about-header-box-two")
 
+
+
+
+
+
+/*  Vertical extend hover
 function box_one_shorten(){
     box_one.classList.remove("extend")
     setTimeout(() => {
@@ -32,6 +38,7 @@ function box_two_extend(){
 }
 $(box_one).hover(()=>{box_one_extend()}, ()=>{box_one_shorten()})
 $(box_two).hover(()=>{box_two_extend()}, ()=>{box_two_shorten()})
+*/
 /*           Click Version
 box_one.addEventListener("click", ()=>{
     if(box_one.classList.contains("extend")){
