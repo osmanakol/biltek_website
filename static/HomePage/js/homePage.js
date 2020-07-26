3ed962f43a1d2c275b81da22e07bc397dbb12d64
arzuozkan <16050111051@ybu.edu.tr>
2020-07-26 15:51:51 +0300

    edit register button

--- static/HomePage/js/homePage.js
+++ static/HomePage/js/homePage.js
@@ -1,12 +1,11 @@
-var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos < currentScrollPos) {
-    document.getElementById("homepage-social-media-bar-register-btn").style.animation = "disappear-register-button 1s 1 both ease-out";
-  } else if(currentScrollPos==0){
-    document.getElementById("homepage-social-media-bar-register-btn").style.animation = "appear-register-button 1s 1 both ease-out";
+  if (currentScrollPos > 70) {
+    document.getElementById("homepage-social-media-bar-register-btn-a").style.animation = "disappear-register-button 1s 1 both ease-out";
+  } else if(currentScrollPos<70){
+    document.getElementById("homepage-social-media-bar-register-btn-a").style.animation = "appear-register-button 1s 1 both ease-out";
   }
-  prevScrollpos = currentScrollPos;
+  /*prevScrollpos = currentScrollPos;*/
 }
 
 /*MOBILENAV JS
