d0f56e3befd5cc890be09e317441ecf709599ac8
Auskren <kbberkuk@gmail.com>
2020-08-06 19:42:52 +0300

    fixed signIn button (!)

--- static/HomePage/js/homePage.js
+++ static/HomePage/js/homePage.js
@@ -1,9 +1,18 @@
+const signInButtonText = document.querySelector(".homepage-social-media-bar-register-btn-a-content")
+
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (currentScrollPos > 70) {
-    document.getElementById("homepage-social-media-bar-register-btn-a").style.animation = "disappear-register-button 1s 1 both ease-out";
+    signInButtonText.classList.toggle("active");
+    document.getElementById("homepage-social-media-bar-register-btn-a").style.animation = "disappear-register-button 1s 1 forwards ease-out";
+    /*setTimeout(() => {
+      if(currentScrollPos > 70){
+        document.getElementById("homepage-social-media-bar-register-btn-a").style.display = "none";}
+    }, 1000);*/
   } else if(currentScrollPos<70){
-    document.getElementById("homepage-social-media-bar-register-btn-a").style.animation = "appear-register-button 1s 1 both ease-out";
+    signInButtonText.classList.remove("active");/*
+    document.getElementById("homepage-social-media-bar-register-btn-a").style.display = "block";*/
+    document.getElementById("homepage-social-media-bar-register-btn-a").style.animation = "appear-register-button 1s 1 forwards ease-out";
   }
   /*prevScrollpos = currentScrollPos;*/
 }
