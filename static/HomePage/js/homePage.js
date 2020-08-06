b4b8eea64dce402f7bcfe738ece1db8820468b79
arzuozkan <16050111051@ybu.edu.tr>
2020-08-06 20:50:15 +0300

    fix register button animation problem

--- static/HomePage/js/homePage.js
+++ static/HomePage/js/homePage.js
@@ -1,17 +1,17 @@
-const signInButtonText = document.querySelector(".homepage-social-media-bar-register-btn-a-content")
+/*const signInButtonText = document.querySelector(".homepage-social-media-bar-register-btn-a-content")*/
 
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
   if (currentScrollPos > 70) {
-    signInButtonText.classList.toggle("active");
+    /*signInButtonText.classList.toggle("active");*/
     document.getElementById("homepage-social-media-bar-register-btn-a").style.animation = "disappear-register-button 1s 1 forwards ease-out";
     /*setTimeout(() => {
       if(currentScrollPos > 70){
         document.getElementById("homepage-social-media-bar-register-btn-a").style.display = "none";}
     }, 1000);*/
   } else if(currentScrollPos<70){
-    signInButtonText.classList.remove("active");/*
-    document.getElementById("homepage-social-media-bar-register-btn-a").style.display = "block";*/
+    /*signInButtonText.classList.remove("active");*/
+    /*document.getElementById("homepage-social-media-bar-register-btn-a").style.display = "block";*/
     document.getElementById("homepage-social-media-bar-register-btn-a").style.animation = "appear-register-button 1s 1 forwards ease-out";
   }
   /*prevScrollpos = currentScrollPos;*/
