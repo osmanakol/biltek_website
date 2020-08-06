ebc6ded607d9c6e4d51e16a20367e07bb23a0522
Auskren <kbberkuk@gmail.com>
2020-08-06 19:53:57 +0300

    added photo animation

--- static/HomePage/js/photos.js
+++ static/HomePage/js/photos.js
@@ -3,6 +3,7 @@ function currentPhoto(photoIndex){
     console.log(photos.length);
     for(var i=0;i<photos.length;i++){
         if(i==photoIndex-1){
+            photos[i].style.animation = "move-photo 1s both ease"
             photos[i].style.display="block";
         }
         else{
