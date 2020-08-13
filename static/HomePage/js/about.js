e0c0e5980bdf11583249b7df65539f828ab46138
Auskren <kbberkuk@gmail.com>
2020-08-13 22:11:36 +0300

    added image animations to about

--- static/HomePage/js/about.js
+++ static/HomePage/js/about.js
@@ -1,6 +1,7 @@
 const box_one = document.querySelector(".homepage-about-header-box-one")
 const box_two = document.querySelector(".homepage-about-header-box-two")
-
+const arrow = document.querySelector(".homepage-about-header-box-one-mission")
+const eye = document.querySelector(".homepage-about-header-box-two-eye")
 
 
 
@@ -9,25 +10,29 @@ const box_two = document.querySelector(".homepage-about-header-box-two")
 /*  Vertical extend hover*/
 function box_one_shorten(){
     box_one.classList.remove("extend")
+    box_two.classList.remove("extend")
+    arrow.classList.toggle("active")
     setTimeout(() => {
         box_two.classList.remove("move")
     }, 100); 
 }
 function box_one_extend(){
     box_two.classList.toggle("move")
+    arrow.classList.toggle("active") 
     setTimeout(() => {
         box_one.classList.toggle("extend")
     }, 100); 
 }
 function box_two_shorten(){
-    box_one.classList.remove("extend")
     box_two.classList.remove("extend")
+    eye.classList.toggle("active")
     setTimeout(() => {
         box_one.classList.remove("move")
     }, 100); 
 }
 function box_two_extend(){
     box_one.classList.toggle("move")
+    eye.classList.toggle("active")
     setTimeout(() => {
         box_two.classList.toggle("extend")
     }, 100);  
