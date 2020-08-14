0586f0a1b46704c91a9e2df2753c5971c1a9faaf
Auskren <kbberkuk@gmail.com>
2020-08-14 14:16:22 +0300

    added about button tooltips

--- static/HomePage/js/about.js
+++ static/HomePage/js/about.js
@@ -2,9 +2,7 @@ const box_one = document.querySelector(".homepage-about-header-box-one")
 const box_two = document.querySelector(".homepage-about-header-box-two")
 const arrow = document.querySelector(".homepage-about-header-box-one-mission")
 const eye = document.querySelector(".homepage-about-header-box-two-eye")
-
-
-
+const buttons = document.querySelectorAll(".about-button")
 
 
 /*  Vertical extend hover*/
@@ -16,6 +14,7 @@ function box_one_shorten(){
         box_two.classList.remove("move")
     }, 100); 
 }
+
 function box_one_extend(){
     box_two.classList.toggle("move")
     arrow.classList.toggle("active") 
@@ -23,6 +22,7 @@ function box_one_extend(){
         box_one.classList.toggle("extend")
     }, 100); 
 }
+
 function box_two_shorten(){
     box_two.classList.remove("extend")
     eye.classList.toggle("active")
@@ -30,6 +30,7 @@ function box_two_shorten(){
         box_one.classList.remove("move")
     }, 100); 
 }
+
 function box_two_extend(){
     box_one.classList.toggle("move")
     eye.classList.toggle("active")
@@ -37,32 +38,13 @@ function box_two_extend(){
         box_two.classList.toggle("extend")
     }, 100);  
 }
+
 $(box_one).hover(()=>{box_one_extend()}, ()=>{box_one_shorten()})
 $(box_two).hover(()=>{box_two_extend()}, ()=>{box_two_shorten()})
-/*           Click Version
-box_one.addEventListener("click", ()=>{
-    if(box_one.classList.contains("extend")){
-         box_one_shorten()
-    }
-    else if(box_two.classList.contains("extend")){
-        box_one.classList.toggle("extend")
-        box_two.classList.remove("extend")
-    }
-    else{
-        box_one_extend()
-    }
-})
 
-box_two.addEventListener("click", ()=>{
-    if(box_two.classList.contains("extend")){
-        box_two_shorten()
-    }
-    else if(box_one.classList.contains("extend")){
-        box_one.classList.remove("extend")
-        box_two.classList.toggle("extend")
-    }
-    else{
-        box_two_extend()   
-    }
-})
-*/
+
+/*  FOR ABOUT BOX TOOLTIPS */
+
+$(buttons).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+  });
