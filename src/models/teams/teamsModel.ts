83868ed0b29d01368f016530e5b6f6498d89853c
velikrgl <velikrgl@yahoo.com>
2020-09-11 15:36:20 +0300

    teamsModel created

--- src/models/teams/teamsModel.ts
+++ src/models/teams/teamsModel.ts
@@ -1,12 +1,25 @@
-import { checkSchema } from "express-validator"
+import { ParticipantModel } from "../participants/participantModel"
+
+
+interface IProject {
+    projectName: string;
+    description: string;
+    year: number;
+}
+type role = "Leader" | "Member"
+
+interface ITeamMember {
+    teamMember: ParticipantModel;
+    year: number
+    role: role;
+}
+
+export class TeamsModel {
+    constructor(public teamName: string, public teamMember: ITeamMember[], 
+        public foundationYear: number = new Date().getFullYear(), public isActive: boolean = true) {
+
+    }
 
 
 
-export class TeamsModule{
-
-public date:string;
-
-
-
-    
 }
