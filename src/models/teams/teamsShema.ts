cf91cec7d9e74923732dfe084dca83950b3e799f
velikrgl <velikrgl@yahoo.com>
2020-09-11 16:01:52 +0300

    team shema created

---
+++ src/models/teams/teamsShema.ts
@@ -0,0 +1,25 @@
+import { Document, Model, model, Schema } from "mongoose";
+
+export interface ITeam extends Document {
+    
+}
+
+
+const teamSchema: Schema = new Schema({
+   teamName:{type:String, required:"Team name is required",unique:true},
+   teamMembers:[{
+       year:{type:Number,default:new Date().getFullYear},
+       members:[{
+           _id:{type:Schema.Types.ObjectId,ref:"participants"},
+           role:{type:String ,required:"Role is required",default:"Member"}
+       }]
+   }] ,
+   foundationYear:{type:Number,required:"Year is required",default:new Date().getFullYear},
+isActive:{type:Boolean ,required:"Activated state is required",default:true}
+
+
+
+}) 
+const TeamDbModel: Model<ITeam> = model('teams', teamSchema);
+ 
+export default TeamDbModel 
