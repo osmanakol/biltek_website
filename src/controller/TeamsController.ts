067e45438b18e19ef25e4f142d1dc177c3e00832
velikrgl <velikrgl@yahoo.com>
2020-09-19 14:15:45 +0300

    teams controller done

--- src/controller/TeamsController.ts
+++ src/controller/TeamsController.ts
@@ -24,6 +24,82 @@ export class TeamsController {
         })
     }
 
+    public createManyParticipant = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.teamService.createMany(new Array<TeamsModel>(...req.body.participants))
+            res.status(201).json({
+                data: result,
+                state: "Success"
+            })
+        } catch (error) {
+            res.json({
+                err: error,
+                state: "Error"
+            })
+        }
+    }
+
+    public updateParticipant = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.teamService.update(req.body._id, new TeamsModel(req.body.name, req.body.surName, req.body.universityId, req.body.departmentId, req.body.email))
+            res.status(200).json({
+                data: result,
+                state: "Success"
+            })
+        } catch (error) {
+            res.json({
+                err: error,
+                state: "Error"
+            })
+        }
+    }
+
+    public deleteParticipant = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.teamService.delete(req.body._id)
+            res.status(200).json({
+                result: result,
+                state: "Success"
+            })
+        } catch (error) {
+            res.json({
+                err: error,
+                state: "Error"
+            })
+        }
+    }
+
+    public findAll = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.teamService.findAll()
+            res.json({
+                data: result,
+                state: "Success"
+            })
+        } catch (error) {
+            res.json({
+                err: error,
+                state: "Error"
+            })
+        }
+    }
+
+    public findById = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const result = await this.teamService.findById(req.body._id)
+            res.json({
+                data: result,
+                state: "Success"
+            })
+        } catch (error) {
+            res.json({
+                err: error,
+                state: "Error"
+            })
+        }
+    }
+}
+
 
    
 }
