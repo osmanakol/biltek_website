0862931717236c99c782f821fd3ac996fe144249
arzuozkan <16050111051@ybu.edu.tr>
2020-09-19 15:50:20 +0300

    fixed http request logger remote address and time

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -2,6 +2,7 @@ import {createLogger,transports,format} from "winston"
 import morgan from "morgan"
 import path from "path"
 import fs from "fs"
+import moment from "moment"
 const {combine,timestamp,colorize,printf}=format
 
 const timeformat={
@@ -32,13 +33,20 @@ const logConfiguration = {
   ]
 } 
 export const logger = createLogger(logConfiguration);
-export const httpLogger=morgan(':remote-addr - :remote-user :date[iso] ":method :url HTTP/:http-version" :status :response-time ms',
-{stream:fs.createWriteStream(path.join(__dirname, 'access.log'))})
+
+morgan.token('remote-addr',function (req, res) { if (req.headers['x-forwarded-for']==undefined) return req.connection.remoteAddress; else return req.headers['x-forwarded-for'].toString()  })
+morgan.token('date', ()=> {
+  return moment().format(timeformat.format);
+ })
+
+ //çıktı formati
+ //:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"
+export const httpLogger=morgan('combined',
+{stream:fs.createWriteStream(path.join(__dirname, 'access.log'),{ flags: 'a' })})
+
 
 /*export class LoggerStream {
   write(message: string) {
       logger.info(message);
   }
-}
-export const httpLogger=morgan(':remote-addr - :remote-user [:date ":method :url HTTP/:http-version" :status',{stream:new LoggerStream()})
-*/
+}*/
