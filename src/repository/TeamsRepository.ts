1741f2898f5a334b0db597dda74805092d207f55
velikrgl <velikrgl@yahoo.com>
2020-09-19 13:46:00 +0300

    add members

--- src/repository/TeamsRepository.ts
+++ src/repository/TeamsRepository.ts
@@ -2,5 +2,5 @@ import { BaseRepository } from "./base/BaseRepository";
 import { TeamsModel } from "../models/teams/teamsModel";
 
 export class TeamsRepository extends BaseRepository<TeamsModel>{
-
+// add members,
 }
