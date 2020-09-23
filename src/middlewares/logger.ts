6d2ac78f9850f1a83aaa2e53a2c056f9d42e4ac3
arzuozkan <16050111051@ybu.edu.tr>
2020-09-23 06:41:29 +0300

    changed log format

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -6,7 +6,7 @@ import moment from "moment"
 const {combine,timestamp,colorize,printf}=format
 
 const timeformat={
-  'format': 'YYYY-MM-DD HH:mm' 
+  'format': 'YYYY-MM-DD HH:mm:ss' 
 }
 
 const logConfiguration = {
@@ -34,14 +34,17 @@ const logConfiguration = {
 } 
 export const logger = createLogger(logConfiguration);
 
-morgan.token('remote-addr',function (req, res) { if (req.headers['x-forwarded-for']==undefined) return req.connection.remoteAddress; else return req.headers['x-forwarded-for'].toString()  })
+morgan.token('remote-addr',function (req) { 
+  if (req.headers['x-forwarded-for']==undefined) 
+    return req.connection.remoteAddress
+  else return req.headers['x-forwarded-for'].toString() 
+ })
 morgan.token('date', ()=> {
   return moment().format(timeformat.format);
  })
 
  //çıktı formati
- //:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"
-export const httpLogger=morgan('combined',
+export const httpLogger=morgan('[:date] :remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent',
 {stream:fs.createWriteStream(path.join(__dirname, 'access.log'),{ flags: 'a' })})
 
 
