a6aeeb597f88705b7be597d297cd8691a2d7eebb
arzuozkan <16050111051@ybu.edu.tr>
2020-09-24 22:10:42 +0300

    change path log files and add rotation

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,13 +1,17 @@
 import {createLogger,transports,format} from "winston"
+import "winston-daily-rotate-file"
 import morgan from "morgan"
 import path from "path"
 import fs from "fs"
 import moment from "moment"
 const {combine,timestamp,colorize,printf}=format
 
+
 const timeformat={
-  'format': 'YYYY-MM-DD HH:mm:ss' 
+  'format': 'YYYY-MM-DD HH:mm:ss',
+  'dateformat':'YYYY-MM-DD'
 }
+const currentDate=moment().format(timeformat.dateformat)
 
 const logConfiguration = {
   'transports': [
@@ -20,18 +24,39 @@ const logConfiguration = {
         printf(levelStatus => `${levelStatus.timestamp} ${levelStatus.level}: ${levelStatus.message}`)
       )
     }),
-    new transports.File({
+    new transports.DailyRotateFile({
       format: combine(
         timestamp(timeformat),
-        printf(status => `${status.timestamp} ${status.level}: ${status.message}`)
+        printf(status => `[${status.timestamp}] ${status.level.toLocaleUpperCase()}: ${status.message}`)
       ),
-      //level:"info" //sadece hangi level belirtilirse oraya kadar olan levelleri yazıyo
-      filename: path.join(__dirname, "combined.log"),
+      filename: path.join("logs",'%DATE%combined.log'),
+      maxFiles:'7d'
       //maxsize: 5242880, //5MB
 
+    })/*.on("rotate",function(oldFilename, newFilename) {
+      logger.error(oldFilename,"=>",newFilename)
+      console.log(new Date(),oldFilename,newFilename)
+      try{
+        fs.unlinkSync(oldFilename); 
+      }catch(err){
+        logger.error(err)
+        console.log(err)
+        console.log("dosya silinmedi")
+      }})*/
+    ,
+    new transports.File({
+      format:combine(
+        timestamp(timeformat),
+        printf(status => `[${status.timestamp}] ${status.level.toLocaleUpperCase()}: ${status.message}`)
+      ),
+      level:"error",
+      filename:'errors.log',
+      dirname:"logs"
     })
   ]
 } 
+
+
 export const logger = createLogger(logConfiguration);
 
 morgan.token('remote-addr',function (req) { 
@@ -45,11 +70,4 @@ morgan.token('date', ()=> {
 
  //çıktı formati
 export const httpLogger=morgan('[:date] :remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent',
-{stream:fs.createWriteStream(path.join(__dirname, 'access.log'),{ flags: 'a' })})
-
-
-/*export class LoggerStream {
-  write(message: string) {
-      logger.info(message);
-  }
-}*/
+{stream:fs.createWriteStream(path.join("logs", `${currentDate}-access.log`),{ flags: 'a+'})})
