4c01f9e9e76217d6d99ad59d2f57cd020eb46086
Ayşenur Yeter <53858796+aysenuryeter@users.noreply.github.com>
2020-09-29 19:14:16 +0300

    created website-2 scheme

--- src/routes/web-routes.ts
+++ src/routes/web-routes.ts
@@ -18,6 +18,25 @@ export class WebRoutes {
             res.render("site/EventsForm", { layout: "EventsFormLayout" })
         })
 
+        //Website 2
+        this.router.get("/home2", (req: Request, res: Response) => {
+            res.render("site/homepage2", { layout: "homepage2Layout" })
+        })
+        this.router.get("/about", (req: Request, res: Response) => {
+            res.render("site/about", { layout: "aboutLayout" })
+        })
+        this.router.get("/teams", (req: Request, res: Response) => {
+            res.render("site/teams", { layout: "teamsLayout" })
+        })
+        this.router.get("/events", (req: Request, res: Response) => {
+            res.render("site/events", { layout: "eventsLayout" })
+        })
+        this.router.get("/gallery", (req: Request, res: Response) => {
+            res.render("site/gallery", { layout: "galleryLayout" })
+        })
+        this.router.get("/contact", (req: Request, res: Response) => {
+            res.render("site/contact", { layout: "contactLayout" })
+        })
         return this.router;
     }
 }
