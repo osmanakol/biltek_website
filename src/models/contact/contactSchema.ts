b783c085583a81504269d74917854bde832c4bd7
arzuozkan <16050111051@ybu.edu.tr>
2020-11-08 13:58:11 +0300

    create contact model

---
+++ src/models/contact/contactSchema.ts
@@ -0,0 +1,24 @@
+import { Document, Model, model, Schema } from "mongoose";
+import { ContactModel } from "./contactModel";
+
+export interface IContact extends Document{
+        address:string,
+        phone?:string,
+        email:string,
+        socialmedias:Map<string,string>
+}
+
+
+const ContactSchema:Schema=new Schema({
+    address:{type:String},
+    phone:{type:String},
+    email:{type:String,required:"Email info is required for contact",unique:true,},
+    socialmedias:{type:Map,of:String},
+
+
+
+})
+
+const ContactDbModel: Model<IContact>=model("contact",ContactSchema)
+
+export default ContactDbModel
