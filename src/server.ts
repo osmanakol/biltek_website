d08d80c63099e0bbb49c0c2d6d6915ca6765676e
Osman Akol <osmanakol@outlook.com>
2021-04-04 20:20:05 +0300

    add nginx server listen configuration

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import app from "./app";
-import { HOST,PORT } from "./config";
+import { HOST, PORT } from "./config";
 import { logger } from "./middlewares/logger";
- /**
-  * @param port
-  * @param host
-  * ? create server for your app
-  */
+import fs from "fs";
+/**
+ * @param port
+ * @param host
+ * ? create server for your app
+ */
 const port2 = PORT || 3003;
-app.set("port", 4445)
-app.listen(app.get('port'),()=>{
-  logger.info("winston and morgan module is using for logging")
-  logger.info("http requests logs are in access.log file")
-  logger.info(`Server Çalışıyor, http://${HOST}:${app.get('port')}`)
+app.set("port", 4445);
+app.listen(app.get("port"), () => {
+  if (process.env.DYNO) {
+    console.log("Running on Heroku...");
+    fs.openSync("/tmp/app-initialized", "w");
+  }
+  console.log("Node app is running on port:", app.get("port"));
+  console.log(`http://localhost:${app.get("port")}`);
+  logger.info("winston and morgan module is using for logging");
+  logger.info("http requests logs are in access.log file");
+  logger.info(`Server Çalışıyor, http://${HOST}:${app.get("port")}`);
 });
