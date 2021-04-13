6752545da85151c0b1ec101939387975fb1bc1af
Osman Akol <osmanakol@outlook.com>
2021-04-13 19:54:01 +0300

    cors policy update

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,11 @@ import { CtfRoutes } from "./routes/ctf-routes";
 import exphbs from "express-handlebars";
 import { staticFile, staticFile2 } from "./config";
 import { httpLogger } from "./middlewares/logger";
-import path from 'path';
+import path from "path";
 import cors from "cors";
 class App {
   public app: Application;
-  public ctf:Application;
+  public ctf: Application;
   public router: express.Router;
   public webrouter: express.Router;
   constructor() {
@@ -21,7 +21,7 @@ class App {
     this.webrouter = express.Router();
     this.handlebars();
     this.config();
-    this.virtualHost()
+    this.virtualHost();
     this.routeConfig();
     this.mongoSetup();
   }
@@ -33,47 +33,57 @@ class App {
     this.app.engine("handlebars", exphbs());
     this.app.set("view engine", "handlebars");
 
-    const ctf_views_path = path.resolve("subdomain", "ctf-page", "views") 
+    const ctf_views_path = path.resolve("subdomain", "ctf-page", "views");
     //path.join(__dirname) + "/subdomain/ctf-page/views"
-    this.ctf.set('views', ctf_views_path)
+    this.ctf.set("views", ctf_views_path);
     const ctf_hbs = exphbs.create({
-      layoutsDir: ctf_views_path + '/layouts',
-      partialsDir: ctf_views_path + '/partials',
-      extname: '.handlebars'
-    })
+      layoutsDir: ctf_views_path + "/layouts",
+      partialsDir: ctf_views_path + "/partials",
+      extname: ".handlebars",
+    });
     this.ctf.engine("handlebars", ctf_hbs.engine);
     this.ctf.set("view engine", "handlebars");
   };
 
   private config = () => {
-    this.app.use(cors({
+    /*this.app.use(cors({
       credentials: true,
       origin: '*'
     }))
     this.ctf.use(cors({
       credentials: true,
       origin: '*'
-    }))
-    this.app.use("/main",express.static(staticFile));
-    console.info(path.resolve("subdomain", "ctf-page", "assets", "."))
-    this.ctf.use("/static" ,express.static(staticFile2))
+    }))*/
+    this.app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      next();
+    });
+    this.app.use("/main", express.static(staticFile));
+    console.info(path.resolve("subdomain", "ctf-page", "assets", "."));
+    this.ctf.use("/static", express.static(staticFile2));
     this.app.use(express.json());
     this.app.use(
       express.urlencoded({
-        extended: true
+        extended: true,
       })
     );
-    this.ctf.use(express.json())
-    this.ctf.use(express.urlencoded({
-      extended: true
-    }))
+    this.ctf.use(express.json());
+    this.ctf.use(
+      express.urlencoded({
+        extended: true,
+      })
+    );
   };
 
   private virtualHost = () => {
-    const domain ="mysite.local"
-      //process.env.NODE_ENV === "production" ? "aybubiltek.com" : "mysite.local";
-    
-    this.app.use(vhost(`ctf.${domain}`, this.ctf))
+    const domain = "mysite.local";
+    //process.env.NODE_ENV === "production" ? "aybubiltek.com" : "mysite.local";
+
+    this.app.use(vhost(`ctf.${domain}`, this.ctf));
   };
 
   private routeConfig = () => {
