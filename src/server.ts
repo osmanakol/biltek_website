070bc299d2c14d98d1d1001f9942fe44fa2c2c25
Osman Akol <osmanakol@outlook.com>
2021-04-13 13:30:10 +0300

    add multi process for performance improvement

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,29 @@
 import app from "./app";
 import { HOST,PORT } from "./config";
 import { logger } from "./middlewares/logger";
+import { cpus } from "os";
+import cluster from "cluster";
  /**
   * @param port
   * @param host
   * ? create server for your app
   */
-const port2 = PORT || 3003;
-app.set("port", 4445)
-app.listen(app.get('port'),()=>{
-  logger.info("winston and morgan module is using for logging")
-  logger.info("http requests logs are in access.log file")
-  logger.info(`Server Çalışıyor, http://${HOST}:${app.get('port')}`)
-});
+
+if(cluster.isMaster){
+  for (let index = 0; index < cpus().length; index++) {
+    cluster.fork()
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    cluster.fork()
+  })
+} else {
+  const port2 = PORT || 3003;
+  app.set("port", 4443)
+  app.listen(app.get('port'),()=>{
+    logger.info("winston and morgan module is using for logging")
+    logger.info("http requests logs are in access.log file")
+    logger.info(`Server Çalışıyor, http://${HOST}:${app.get('port')}`)
+  });
+}
+
