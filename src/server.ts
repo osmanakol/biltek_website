89dfeae573cad7b152ba2bdba3004d6f5f1e07bb
Osman Akol <osmanakol@outlook.com>
2021-04-13 13:59:39 +0300

    nginx cannot bind problem solvedd

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,10 @@ import fs from "fs";
   */
 
 if(cluster.isMaster){
+  if (process.env.DYNO) {
+    console.log("Running on Heroku...");
+    fs.openSync("/tmp/app-initialized", "w");
+  }
   for (let index = 0; index < cpus().length; index++) {
     cluster.fork()
   }
@@ -22,10 +26,7 @@ if(cluster.isMaster){
   const port2 = PORT || 3003;
   app.set("port", PORT)
   app.listen(app.get('port'),()=>{
-    if (process.env.DYNO) {
-      console.log("Running on Heroku...");
-      fs.openSync("/tmp/app-initialized", "w");
-    }
+  
     logger.info("winston and morgan module is using for logging")
     logger.info("http requests logs are in access.log file")
     logger.info(`Server Çalışıyor, http://${HOST}:${app.get('port')}`)
