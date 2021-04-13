6a8819b569f4e44799f30cf5b1fded3526146a4d
Osman Akol <osmanakol@outlook.com>
2021-04-13 15:01:40 +0300

    for test back the previous version

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import fs from "fs";
  * ? create server for your app
  */
 let count = 0;
-if (cluster.isMaster) {
+/*if (cluster.isMaster) {
   for (let index = 0; index < cpus().length; index++) {
     cluster.fork();
   }
@@ -31,4 +31,16 @@ if (cluster.isMaster) {
     logger.info("http requests logs are in access.log file");
     logger.info(`Server Çalışıyor, http://${HOST}:${app.get("port")}`);
   });
-}
+}*/
+const port2 = PORT || 3003;
+  app.set("port", 4445);
+  app.listen(app.get("port"), "0.0.0.0", () => {
+    if (process.env.DYNO && count == 0) {
+      console.log("Running on Heroku...");
+      fs.openSync("/tmp/app-initialized", "w");
+      count++;
+    }
+    logger.info("winston and morgan module is using for logging");
+    logger.info("http requests logs are in access.log file");
+    logger.info(`Server Çalışıyor, http://${HOST}:${app.get("port")}`);
+  });
