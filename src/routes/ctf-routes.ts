cd8aaaa2ad4a31646480d69be117f3ada0343b74
Osman Akol <osmanakol@outlook.com>
2021-06-06 11:34:46 +0300

    BLAB-41 merge request

--- src/routes/ctf-routes.ts
+++ src/routes/ctf-routes.ts
@@ -11,89 +11,11 @@ export class CtfRoutes {
       res: Response,
       next: NextFunction
     ) {
-      res.render("site/homepage", {
+      res.render("site/index", {
         layout: "indexLayout",
         title: "AnaSayfa",
-        class: "homepage",
+        class:"homepage",
         cssName: "index",
-      });
-    });
-
-    this.route.get("/cyberbiltek", function (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) {
-      res.render("site/cyberbiltek", {
-        layout: "indexLayout",
-        title: "BTG Nedir?",
-        class: "cyberbiltek",
-        cssName: "cyberbiltek",
-      });
-    });
-
-    this.route.get("/history", function (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) {
-      res.render("site/history", {
-        layout: "indexLayout",
-        title: "History",
-        class: "history",
-        cssName: "history",
-      });
-    });
-
-    this.route.get("/ctf", function (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) {
-      res.render("site/ctf", {
-        layout: "indexLayout",
-        title: "CTF",
-        class: "ctf",
-        cssName: "ctf",
-      });
-    });
-
-    this.route.get("/calender", function (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) {
-      res.render("site/calender", {
-        layout: "indexLayout",
-        title: "Calender",
-        class: "calender",
-        cssName: "calender",
-      });
-    });
-
-    this.route.get("/sponsor", function (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) {
-      res.render("site/sponsor", {
-        layout: "indexLayout",
-        title: "Sponsor",
-        class: "sponsor",
-        cssName: "sponsor",
-      });
-    });
-
-    this.route.get("/register", function (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) {
-      res.render("site/register", {
-        layout: "indexLayout",
-        title: "Register",
-        class: "register",
-        cssName: "register",
       });
     });
 
